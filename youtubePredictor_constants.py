2c11d57c420665a275a86765f47e99685a9f8e32
anthemwingate <49780762+anthemwingate@users.noreply.github.com>
2021-03-21 21:29:05 -0400

    Update youtubePredictor_constants.py

--- youtubePredictor_constants.py
+++ youtubePredictor_constants.py
@@ -20,6 +20,7 @@ YOUTUBE_PREDICTOR_APP_PORT = 8080
 YOUTUBE_PREDICTOR_APP_HOST = '0.0.0.0'
 URL_TEMPLATE = 'wss://{}/speech-to-text/api/v1/recognize'
 UPLOAD_FOLDER_PATH = 'save/'
+PACKAGE_VERSION = 0.0.1
 
 
 # Credentials
