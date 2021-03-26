1a5b71cca6438a5dce6188824af4b62f63c84ff2
Sabetpour Wingate Technologies <49780762+anthemwingate@users.noreply.github.com>
2021-03-26 13:27:38 -0400

    Expanding environment parameters for comprehensive testing

---
+++ tests/this_is_a_test.py
@@ -0,0 +1,24 @@
+# Project                                                  : DiTTo Yoututbe Predictor
+# Author                                                   : Team DiTTo Stevens Institute of Tecchnology SSW 695A Spring 2021
+# Architect/Core Implementation                            : Anthem Rukiya J. Wingate
+# Architect/Production Design                              : Hanqing Liu
+# Version Control Management and Quality Assurance Tester  : Farnaz Sabetpour
+# Purpose                                                  : Flask APP to run Youtube Predictor Implementation
+# Revision History                                         : Version 1.0
+
+# Notes:
+#
+#   dummy method for testing environment
+#
+#
+
+import unittest
+
+
+class NotReallyATestCase(unittest.TestCase):
+    def test_nothing(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
