9c2e29a6a3fb6131aca14a83f49929c62e326623
Sabetpour Wingate Technologies <49780762+anthemwingate@users.noreply.github.com>
2021-03-26 14:13:39 -0400

    reorg

--- tests/this_is_a_test.py
+++ tests/this_is_a_test.py
@@ -13,12 +13,42 @@
 #
 
 import unittest
+import os
+import coverage
 
 
 class NotReallyATestCase(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        """ Class setup  """
+        self.dir_abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        self.directory_path = f"{self.dir_abs_path}/tests/"
+        return super().setUp()
+
     def test_nothing(self):
+        print(self.directory_path)
         self.assertEqual(True, True)
+
+    def teardown(self):
+        """ Class tear down  """
+
+        self.dir_abs_path.dispose()
+        self.directory_path.dispose()
+        self.data_mgr.dispose()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    cov = coverage.Coverage()
+    cov.start()
+    unittest.NotReallyATestCase.maxDiff = None
+    unittest.main(verbosity=2)    print("\nTesting Concluded with result:", test_result)
+    cov.stop()
+    cov.save()
+    print('Coverage Summary: ')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covdir)
+    cov.xml_report()
+    cov.erase()
