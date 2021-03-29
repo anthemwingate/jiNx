f9daa5d00510c7718f955679eb33350f43ef9008
Sabetpour Wingate Technologies <49780762+anthemwingate@users.noreply.github.com>
2021-03-29 13:03:17 -0400

    Stashed changed, may revert

---
+++ gptNeo/youtubePredictory_gptNeo.py
@@ -0,0 +1,15 @@
+
+# Project                                                  : DiTTo Yoututbe Predictor
+# Author                                                   : Team DiTTo Stevens Institute of Tecchnology SSW 695A Spring 2021
+# Architect/Core Implementation                            : Anthem Rukiya J. Wingate
+# Architect/Production Design                              : Hanqing Liu
+# Version Control Management and Quality Assurance Tester  : Farnaz Sabetpour
+# Purpose                                                  : Flask APP to run Youtube Predictor Implementation
+# Revision History                                         : Version 1.0
+
+# Notes:
+#
+#
+#
+#
+
