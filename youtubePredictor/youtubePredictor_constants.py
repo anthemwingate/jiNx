6b56491ad4e8d9ba2888813ab6e882a41bad58fb
anthemwingate <49780762+anthemwingate@users.noreply.github.com>
2021-04-04 00:45:59 -0400

    Update youtubePredictor_constants.py

--- youtubePredictor/youtubePredictor_constants.py
+++ youtubePredictor/youtubePredictor_constants.py
@@ -26,8 +26,8 @@ LOG_FILE_PATH = "./logs/youtubePredictor.log"
 YOUTUBE_EXTRACTOR_KEY = 'Youtube'
 
 # Credentials
-TONE_ANALYZER_API_KEY='MpLSJJOCohXvxa4-qLqnLao0hRT6y7jPr1HG5mUgKCmB'
-TONE_ANALYZER_API_URL='https://api.us-east.tone-analyzer.watson.cloud.ibm.com/instances/6232e289-7948-40ca-9205-bd8d98f5a847'
+TONE_ANALYZER_API_KEY='insert key'
+TONE_ANALYZER_API_URL='insert url'
 TONE_ANALYZER_VERSION = '2017-09-21'
 
 # DataBase Queries
