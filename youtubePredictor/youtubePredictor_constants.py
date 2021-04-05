a83e646e675e755a3353d78f62eb0d47b4827775
Sabetpour Wingate Technologies <49780762+anthemwingate@users.noreply.github.com>
2021-04-04 22:29:30 -0400

    gluing together demo

--- youtubePredictor/youtubePredictor_constants.py
+++ youtubePredictor/youtubePredictor_constants.py
@@ -26,9 +26,7 @@ LOG_FILE_PATH = "./logs/youtubePredictor.log"
 YOUTUBE_EXTRACTOR_KEY = 'Youtube'
 
 # Credentials
-TONE_ANALYZER_API_KEY='MpLSJJOCohXvxa4-qLqnLao0hRT6y7jPr1HG5mUgKCmB'
-TONE_ANALYZER_API_URL='https://api.us-east.tone-analyzer.watson.cloud.ibm.com/instances/6232e289-7948-40ca-9205-bd8d98f5a847'
-TONE_ANALYZER_VERSION = '2017-09-21'
+
 
 # DataBase Queries
 CSV_FILE_COLUMN_NAMES = ["ID",
